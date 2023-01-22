4e65796c7ec534d56341703484ab5f4426388633
zeroalef <zeroalef@protonmail.com>
2023-01-22 02:41:04 +0200

    add LinkedList public interface add thread save LinkedList public interface

--- src/list/imperative_list.rs
+++ src/list/imperative_list.rs
@@ -1,6 +1,7 @@
 use std::borrow::BorrowMut;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
+use std::sync::{Arc, Mutex};
 
 type Link<T> = Option<NonNull<OneLinkNode<T>>>;
 
@@ -9,7 +10,54 @@ struct OneLinkNode<T> {
     data: T,
 }
 
-pub struct LinkedListInner<T> {
+/// One-linked list implementation also known as `stack`.
+/// Fully imperative implementation using pointers for faster.
+
+/// not thread safety list
+pub struct LinkedList<T>(LinkedListInner<T>);
+
+impl<T> LinkedList<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self(LinkedListInner::new())
+    }
+    #[inline]
+    pub fn push(&mut self, data: T) {
+        self.0.cons(data);
+    }
+    #[inline]
+    pub fn pop(&mut self) -> Option<T> {
+        self.0.car()
+    }
+    #[inline]
+    pub fn rest(&self) -> Self {
+        Self(self.0.cdr())
+    }
+}
+
+/// thread safety list
+pub struct TSLinkedList<T>(Arc<Mutex<LinkedListInner<T>>>);
+
+impl<T> TSLinkedList<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self(Arc::new(Mutex::new(LinkedListInner::new())))
+    }
+    #[inline]
+    pub fn push(&mut self, data: T) {
+        self.0.lock().unwrap().cons(data);
+    }
+    #[inline]
+    pub fn pop(&self) -> T {
+        self.0.lock().unwrap().car()
+    }
+    #[inline]
+    pub fn rest(&self) -> Self {
+        Self(Arc::new(Mutex::new(self.0.lock().unwrap().cdr())))
+    }
+}
+
+struct LinkedListInner<T> {
     head: Link<T>,
     len: usize,
     _marker: PhantomData<T>,
@@ -27,10 +75,8 @@ impl<T> LinkedListInner<T> {
     #[inline(always)]
     pub fn cons(&mut self, data: T) {
         unsafe {
-            let new_node = NonNull::new_unchecked(Box::into_raw(Box::new(OneLinkNode {
-                next: None,
-                data,
-            })));
+            let new_node =
+                NonNull::new_unchecked(Box::into_raw(Box::new(OneLinkNode { next: None, data })));
             (*new_node.as_ptr()).next = self.head;
             self.head = Some(new_node);
             self.len += 1;
@@ -67,10 +113,10 @@ mod test {
     #[test]
     fn test_linked_list() {
         let mut l: LinkedListInner<i32> = LinkedListInner::new();
-        (0..100).into_iter().for_each(|value| {
+        (0..1000000).into_iter().for_each(|value| {
             l.cons(value);
         });
-        (0..100).into_iter().rev().for_each(|value| {
+        (0..1000000).into_iter().rev().for_each(|value| {
             assert_eq!(l.car(), Some(value));
         });
     }
